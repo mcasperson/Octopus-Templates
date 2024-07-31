19a6006811409cea56d8947f4a01162a9367487c
JChatmasOctopus <50506513+JChatmasOctopus@users.noreply.github.com>
2024-07-30 19:22:51 -0500

    Create project.tf

---
+++ projects/k8s-helm/projects/ChildProject/project.tf
@@ -0,0 +1,49 @@
+terraform {
+
+  required_providers {
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.21.1" }
+  }
+
+  backend "s3" {
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "octopusdeploy" {
+  address  = trimspace(var.octopus_server)
+  api_key  = trimspace(var.octopus_apikey)
+  space_id = trimspace(var.octopus_space_id)
+}
+
+module "template_project" {
+  source = "../../template/space_population"
+  project_k8s_helm_template_name = var.project_name
+  k8s_helm_template_kubernetes_namespace_1 = "savings"
+}
+
+variable "octopus_server" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The URL of the Octopus server e.g. https://myinstance.octopus.app."
+}
+variable "octopus_apikey" {
+  type        = string
+  nullable    = false
+  sensitive   = true
+  description = "The API key used to access the Octopus server. See https://octopus.com/docs/octopus-rest-api/how-to-create-an-api-key for details on creating an API key."
+}
+variable "octopus_space_id" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The ID of the Octopus space to populate."
+}
+variable "project_name" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The name of the project exported from K8s Helm Template"
+  default     = "K8s - Helm"
+}
