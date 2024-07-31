10ca1a53a4177892f23fdff10d13ab36ad3ecf62
JChatmasOctopus <JChatmasOctopus@users.noreply.github.com>
2024-07-31 00:46:19 +0000

    Apply automatic changes

---
+++ projects/k8s-helm/template/space_population/config.tf
@@ -0,0 +1,13 @@
+terraform {
+
+  required_providers {
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.22.0" }
+    shell         = { source = "scottwinkler/shell", version = "1.7.10" }
+    external      = { source = "hashicorp/external", version = "2.3.3" }
+  }
+
+  backend "s3" {
+  }
+
+  required_version = ">= 1.6.0"
+}
