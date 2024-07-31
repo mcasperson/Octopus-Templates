5c3f069876182988eac454403331f7ffc5fed08b
JChatmasOctopus <50506513+JChatmasOctopus@users.noreply.github.com>
2024-07-30 19:30:23 -0500

    Update project.tf

--- projects/k8s-helm/projects/ChildProject/project.tf
+++ projects/k8s-helm/projects/ChildProject/project.tf
@@ -19,7 +19,7 @@ provider "octopusdeploy" {
 module "template_project" {
   source = "../../template/space_population"
   project_k8s_helm_template_name = var.project_name
-  k8s_helm_template_kubernetes_namespace_1 = "savings"
+  k8s___helm_project_packagename_1 = "octopusdeploylabs/octotesting"
 }
 
 variable "octopus_server" {
