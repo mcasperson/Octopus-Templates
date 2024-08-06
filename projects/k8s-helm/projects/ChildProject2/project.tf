2e1742438f657c721307eb9205a4bdce8ddf7225
JChatmasOctopus <50506513+JChatmasOctopus@users.noreply.github.com>
2024-08-06 09:58:56 -0500

    Update project.tf

--- projects/k8s-helm/projects/ChildProject2/project.tf
+++ projects/k8s-helm/projects/ChildProject2/project.tf
@@ -1,7 +1,7 @@
 terraform {
 
   required_providers {
-    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.22.0" }
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.22.1" }
   }
 
   backend "s3" {
